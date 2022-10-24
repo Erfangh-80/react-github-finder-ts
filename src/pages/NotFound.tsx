126cf0a1290e3c65b5b6c9b3394da9dc3755c636
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-10-24 23:36:49 +0330

    create content notfound component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,22 @@
+import { FaHome } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const NotFound = (): JSX.Element => {
-    return (
-        <div>
-            NotFound
+  return (
+    <div className="flex justify-center">
+      <div className="text-center hero-content">
+        <div className="max-w-lg text-white">
+          <h1 className="text-6xl font-bold mb-8">Oops!</h1>
+          <p className="text-3xl mb-6">404 - Page not found!</p>
+          <div className="flex justify-center">
+            <Link to="/" className="flex items-center bg-[#3e2057] hover:bg-[#201143] text-white font-bold py-2 px-4 rounded">
+                <FaHome className="mr-2"/> back to Home
+            </Link>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
