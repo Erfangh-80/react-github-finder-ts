40113d325a716bce96a815ad5a548e81047e7b20
Erfan Gholami <Erfangh4871gholami@gmail.com>
2022-10-24 23:37:18 +0330

    create content About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,15 @@
 const About = (): JSX.Element => {
-    return (
-        <div>
-            About
-        </div>
-    );
+  return (
+    <>
+      <h1 className="text-4xl text-white">Github Finder</h1>
+      <p className="mb-4 text-1xl font-light text-white">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </>
+  );
 };
 
 export default About;
