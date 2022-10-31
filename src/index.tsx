e135616dc908f9393a7c838a35ad0be254dafa65
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2022-10-31 23:02:36 +0330

    create file env and use token in project

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 // css
 import "./index.css";
 // App components
 import App from "./App";
+
+axios.defaults.headers.common["Authorization"] = `token ${process.env.REACT_APP_GITHUB_TOKEN}`
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
