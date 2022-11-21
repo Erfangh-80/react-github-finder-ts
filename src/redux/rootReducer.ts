d7350066fc3b9ab4264ed86b8cda1c43e2c52a8b
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2022-11-21 14:08:08 +0330

    add user redux

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 import usersReducer from "./users/usersReducer"
+import userReducer from "./user/userReducer"
 import alertReducer from "./alert/alertReducer"
 
 const rootReducer = combineReducers({
     usersState: usersReducer,
+    userState: userReducer,
     alertState: alertReducer,
 })
 
