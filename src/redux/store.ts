129e5a478755e396e7c1e720dc1cbd0e84696395
Erfangh-80 <72462171+Erfangh-80@users.noreply.github.com>
2022-11-28 19:30:33 +0330

    use redux deb-tools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,8 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
-
+import { composeWithDevTools } from "@redux-devtools/extension"
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store
